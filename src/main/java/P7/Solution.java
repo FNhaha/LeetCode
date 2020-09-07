4f9700bbc8d9196155dbb225c66bb621a24051de
Nong Fei <171250636@smail.nju.edu.cn>
2020-09-07 11:51:00 +0800

    update P7

--- src/main/java/P7/Solution.java
+++ src/main/java/P7/Solution.java
@@ -1,4 +1,20 @@
 package P7;
 
+import java.util.Stack;
+
 public class Solution {
+    public int reverse(int x) {
+        int offset = x > 0? 1: -1;
+        x = Math.abs(x);
+        int res = 0;
+        while (x > 0) {
+            int n = x % 10;
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && n > 7)) {
+                return 0;
+            }
+            res = res * 10 + n;
+            x /= 10;
+        }
+        return res * offset;
+    }
 }
--- src/test/java/P7/P7Test.java
+++ src/test/java/P7/P7Test.java
@@ -1,4 +1,42 @@
 package P7;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class P7Test {
+    Solution solution = new Solution();
+    @Test
+    public void test1() {
+        assertEquals(123, solution.reverse(321));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(-321, solution.reverse(-123));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(1, solution.reverse(10));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(0, solution.reverse(0));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(1, solution.reverse(1));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(0, solution.reverse(1534236469));
+    }
+
+    @Test
+    public void test7() {
+        assertEquals(0, solution.reverse(-1534236469));
+    }
 }
